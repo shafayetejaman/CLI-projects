ab3679b511d5d96850ed6fb0a9d6af328439cd6f
shafayetejaman <shafayetejaman@gmail.com>
2024-12-18 14:03:14 +0600

    navigating pages script

---
+++ navigation pages/index.js
@@ -0,0 +1,32 @@
+// Get the current URL
+
+// let url = "https://4anime.cc/anime-online/arifureta-shokugyou-de-sekai-saikyou-2nd-season-dub/013";
+let url = new URL(window.location.href);
+
+// Get the path parts as an array
+let pathParts = url.pathname.split('/');
+
+// Find the last part that's a number and increment it
+for (let i = pathParts.length - 1; i >= 0; i--)
+{
+    if (!isNaN(pathParts[i]) && pathParts[i] !== "")
+    {
+        const s = String(parseInt(pathParts[i]) + 1);
+        pathParts[i] = s.padStart(pathParts[i].length, "0");
+        break;
+    }
+}
+
+// Rebuild the new path and set it as the new URL
+url.pathname = pathParts.join('/');
+console.log(pathParts[pathParts.length - 1])
+
+// episode-012";
+if (!document.querySelector(`#episode-${pathParts[pathParts.length - 1]}`)) // Check if the element exists
+{
+    prompt("Not found!");
+    return;
+}
+
+// Redirect to the new URL
+window.location.href = url.href;
