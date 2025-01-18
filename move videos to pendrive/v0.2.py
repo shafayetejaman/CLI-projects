19c949d75dc2bcdba2a3e8102577a18d5f3f6a30
shafayetejaman <shafayetejaman@gmail.com>
2025-01-19 02:39:57 +0600

    file transfer v0.2 completed

--- move videos to pendrive/v0.2.py
+++ move videos to pendrive/v0.2.py
@@ -1,9 +1,8 @@
 from os import path
-from shutil import move
 from glob import glob
-from threading import Thread
 from time import perf_counter
 from concurrent.futures import ThreadPoolExecutor
+from subprocess import run, CalledProcessError
 
 SRC_DIR = "F:\\Downloads\\Video"
 DIST_DIR = "H:\\New folder"
@@ -32,24 +31,15 @@ threads = []
 
 startTime = perf_counter()
 
-# for fileName in allFiles:
-#     distPath = path.join(DIST_DIR, path.basename(fileName))
-
-#     print("copying.........")
-#     thread = Thread(target=move, args=(fileName, distPath))
-#     print(f"{fileName} -> {distPath}")
-
-#     thread.start()
-#     threads.append(thread)
-
-# for thread in threads:
-#     thread.join()
-
 
 def move_file(src):
     print("copying.........")
     dist = path.join(DIST_DIR, path.basename(src))
-    move(src, dist)
+    try:
+        run(["move", src, dist], shell=True, check=True)
+    except CalledProcessError as e:
+        print("Error: ", e)
+
     print(f"Moved {src} -> {dist}")
 
 
@@ -58,3 +48,4 @@ with ThreadPoolExecutor(max_workers=8) as executor:
 
 
 print("complete. Took: ", round(perf_counter() - startTime, 2), "S")
+# 28 second
